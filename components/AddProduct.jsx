e95bdd16d1002c80ae6c0aadc04998b5a1f8bae9
lmambl <lmambl@mail.ru>
2023-11-23 17:22:55 +0300

    Navchik

--- components/AddProduct.jsx
+++ components/AddProduct.jsx
@@ -2,12 +2,12 @@ const React = require('react');
 
 function AddProduct() {
   return (
-    <form className='addProduct'>
-      <input type="text" name="title" />
-      <input type="text" name="url" />
-      <input type="text" name="price" />
-      <input type="text" name="count" />
-      <input type="text" name="status" />
+    <form className="addProduct">
+      <input type="text" name="title" placeholder="title" />
+      <input type="text" name="url" placeholder="url" />
+      <input type="text" name="price" placeholder="price" />
+      <input type="text" name="count" placeholder="count" />
+      <input type="text" name="status" placeholder="status" />
       <button type="submit">Добавить</button>
     </form>
   );
