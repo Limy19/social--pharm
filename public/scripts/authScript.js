cba3830f03c5bd840dd12145bb0e1f59a5687697
Lida <lidamalygina@mail.ru>
2023-11-23 11:30:58 +0300

    getPostRega

---
+++ public/scripts/authScript.js
@@ -0,0 +1,8 @@
+const userRega = document.querySelector('.userRega');
+
+if(userRega){
+    userRega.addEventListener('submit',async(event){
+        const{name, email, password}=event.target
+
+    })
+}
--- routes/api/authApi.routes.js
+++ routes/api/authApi.routes.js
@@ -1,2 +1,20 @@
 const router = require('express').Router();
+const { User } = require('../../db/models');
+const bcrypt = require('bcrypt');
 
+router.post('/registration', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (name && email && password) {
+      await User.create({
+        name,
+        email,
+        password: bcrypt.hash(password, 10),
+        isAdmin: false,
+      });
+    }
+  } catch ({ message }) {
+    res.status(500).end();
+  }
+});
+module.exports = router;
