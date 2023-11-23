d6cf472bb7aaa8fd5a434df9ed9194cfc5a8db79
Lida <lidamalygina@mail.ru>
2023-11-23 16:13:21 +0300

    Regaupdate

--- components/Rega.jsx
+++ components/Rega.jsx
@@ -7,9 +7,15 @@ function Rega({ title }) {
       <form className="userRega">
         <input name="name" placeholder="name" required />
         <input name="email" placeholder="email" required />
-        <input name="password" placeholder="password" required />
+        <input
+          name="password"
+          placeholder="password"
+          required
+          type="password"
+        />
         <button type="submit">Регистрация</button>
       </form>
+      <div className="errRega"></div>
     </Layot>
   );
 }
--- public/scripts/authScript.js
+++ public/scripts/authScript.js
@@ -1,8 +1,26 @@
 const userRega = document.querySelector('.userRega');
+console.log(userRega);
 
-if(userRega){
-    userRega.addEventListener('submit',async(event){
-        const{name, email, password}=event.target
-
-    })
+if (userRega) {
+  userRega.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const { name, email, password } = event.target;
+    const res = await fetch('/api/auth/registration', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: name.value,
+        email: email.value,
+        password: password.value,
+      }),
+    });
+    const data = await res.json();
+    if (data.registration) {
+      window.location.assign(data.url);
+    } else {
+      document.querySelector('.errRega').innerHTML = data.message;
+    }
+  });
 }
